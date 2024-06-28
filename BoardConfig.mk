702b79c890a24f76be0c308e076f53d5a02bc397
Azmathulla Shaik <107968141+IamAzmathullaShaikh@users.noreply.github.com>
2024-06-28 09:40:22 +0530

    Edit: rm -rf Target prebuilt ,Add Defconfig Path , Sepolicy Path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,8 @@
 
 DEVICE_PATH := device/samsung/a12s
 BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+
 
 # Architecture
 TARGET_ARCH := arm64
@@ -24,6 +26,9 @@ TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a15
 
 # APEX
 DEXPREOPT_GENERATE_APEX_IMAGE := true
+
+# system/core and build/make
+AB_OTA_UPDATER := false
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := exynos850
@@ -45,19 +50,17 @@ BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_INCLUDE_DTB_IN_BOOTIMG := true
 BOARD_KERNEL_SEPARATED_DTBO := true
-TARGET_KERNEL_CONFIG := a12s_defconfig
+TARGET_KERNEL_CONFIG := rsuntk_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/a12s
 
-# Kernel - prebuilt
-TARGET_FORCE_PREBUILT_KERNEL := false
-ifeq ($(TARGET_FORCE_PREBUILT_KERNEL),true)
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilts/kernel
-TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilts/dtb.img
-BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
-BOARD_INCLUDE_DTB_IN_BOOTIMG := 
+## Kernel
+BOARD_KERNEL_IMAGE_NAME := Image
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_SOURCE := kernel/samsung/exynos850
+TARGET_KERNEL_CLANG_COMPILE := true
+
+## DTBO
 BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilts/dtbo.img
-BOARD_KERNEL_SEPARATED_DTBO := 
-endif
 
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
@@ -68,6 +71,8 @@ BOARD_SUPER_PARTITION_SIZE := 9126805504 # TODO: Fix hardcoded value
 BOARD_SUPER_PARTITION_GROUPS := samsung_dynamic_partitions
 BOARD_SAMSUNG_DYNAMIC_PARTITIONS_PARTITION_LIST :=
 BOARD_SAMSUNG_DYNAMIC_PARTITIONS_SIZE := 9122611200 # TODO: Fix hardcoded value
+BOARD_USES_METADATA_PARTITION := true
+BOARD_ROOT_EXTRA_FOLDERS := efs
 
 # Platform
 TARGET_BOARD_PLATFORM := universal3830
@@ -91,6 +96,9 @@ TARGET_USERIMAGES_USE_F2FS := true
 # Security patch level
 VENDOR_SECURITY_PATCH := 2021-08-01
 
+## Keymaster
+TARGET_KEYMASTER_VARIANT := samsung
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
@@ -101,6 +109,14 @@ BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 
 # VINTF
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+
+## RIL
+ENABLE_VENDOR_RIL_SERVICE := true
+
+## SELinux
+BOARD_SEPOLICY_TEE_FLAVOR := teegris
+include device/lineage/sepolicy/exynos/sepolicy.mk
+include device/samsung_slsi/sepolicy/sepolicy.mk
 
 # Inherit the proprietary files
 include vendor/samsung/a12s/BoardConfigVendor.mk
